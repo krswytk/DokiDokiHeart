ca19d41126d88ba0bc97455698e600e89de63e94
krswytk <krswytk.0228@gmail.com>
2020-08-16 12:10:38 +0900

    いろいろ追加ー

---
+++ DokiDoki/Assets/Script/OnHeart.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnHeart : MonoBehaviour
+{
+    public Text Htext;
+    public Text Otext;
+    public Image HImage;
+    private float Heart;
+    private float O_2;
+    SerialLight SL;
+
+    ///hertanim
+    [SerializeField] private Sprite[] Hearts;
+    [SerializeField] private float time = 5;//1周にかかる秒数
+    private float timer;
+    private float Atimer;
+    private int num;
+    /// <summary>
+    /// 
+    /// 
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SL = GetComponent<SerialLight>();
+        timer = 0;
+        Atimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Heart = SL.GetH();
+        O_2 = SL.GetO();
+
+        Ontext();//テキスト表記を行う関数
+        timer += Time.deltaTime;
+
+
+        HA(time);//ドキドキを行う関数
+    }
+
+    private void HA(float time)
+    {
+        float s = time / Hearts.Length;//1枚の描写にかけていい時間
+
+        if (timer > Atimer)
+        {
+            Atimer = (timer + s);
+            num++;
+            if (num >= Hearts.Length) num = 0;
+            //Debug.Log("Atimer" + Atimer + "  " + "timer" + timer);
+            HImage.sprite = Hearts[num]; 
+        }
+
+    }
+    private void Ontext()
+    {
+        Htext.text = "Heart:" + Heart.ToString();
+        Otext.text = "  O_2:" + O_2.ToString();
+        //Debug.Log(Heart + " , " + O_2);
+    }
+
+}
+
---
+++ DokiDoki/Assets/Script/Starting.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Starting : MonoBehaviour
+{
+    SerialLight SL;
+    /// 指を置いてください
+    [SerializeField] private GameObject IMOB;
+    [SerializeField] private GameObject HandOB;
+    private Text HandText;
+
+    private float H;
+    private float O;
+
+    private bool on;
+    private bool stand_by;
+    private bool Allstand_by;
+
+
+    private float TotalTimer;
+    private float Totalnum;
+    private int num;
+    private float ave;
+    [SerializeField] private float Limiter = 3;//最初計測しない時間時間
+    [SerializeField] private float LImit = 5;//平均心拍計算までの時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SL = GetComponent<SerialLight>();
+        HandText = HandOB.GetComponent<Text>();
+        HandText.text = "♡お指を置いてね♡";
+        on = false;
+        TotalTimer = 0;
+        Totalnum = 0;
+        stand_by = false;
+        Allstand_by = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        H = SL.GetH();
+        O = SL.GetO();
+
+        if (stand_by == false)
+        {
+            if (on == false) {
+                if (H != 0) {
+                    on = true;
+                    HandText.text = "♡そのまま維持してね♡";
+                    Debug.Log("計測開始");
+                }
+            }//一度値を取ったらスイッチをtrueに
+
+            if (on == true)
+            {
+                TotalTimer += Time.deltaTime;
+                if (TotalTimer > Limiter)
+                {
+                    Debug.Log("計測中");
+                    Totalnum += H;
+                    num++;
+                }
+
+                if (TotalTimer > LImit + Limiter)
+                { 
+                    ave = Totalnum / num;
+                    Debug.Log("AVE : " + ave);
+                    stand_by = true;
+                    HandText.text = "♡OKそのまま維持して♡";
+                    TotalTimer = 0;
+                }
+            }
+        }
+        else
+        {
+            TotalTimer += Time.deltaTime;
+            if (TotalTimer > 3)
+            {
+                OFFIMOB();
+                Allstand_by = true;
+            }
+        }
+
+    }
+    private void OFFIMOB()
+    {
+        IMOB.SetActive(false);
+    }
+    private void ONIMOB()
+    {
+        IMOB.SetActive(true);
+    }
+    public float GetAve()
+    {
+        return ave;
+    }
+    private bool GetST()
+    {
+        return Allstand_by;
+    }
+}
